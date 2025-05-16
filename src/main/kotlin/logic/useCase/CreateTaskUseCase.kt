81512fa65fec76d396ee1c57df514e6be3b45655
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-16 12:38:37 +0300

    refactor: use RxJava for task use cases

--- src/main/kotlin/logic/useCase/CreateTaskUseCase.kt
+++ src/main/kotlin/logic/useCase/CreateTaskUseCase.kt
@@ -1,12 +1,14 @@
 package logic.useCase
 
-import org.example.logic.models.*
-import org.example.logic.repositries.TaskRepository
+import io.reactivex.rxjava3.core.Single
+import org.example.logic.models.AuditLog
+import org.example.logic.models.ProjectState
+import org.example.logic.models.Task
 import org.example.logic.repositries.ProjectStateRepository
+import org.example.logic.repositries.TaskRepository
 import org.example.logic.useCase.CreateAuditLogUseCase
 import org.example.logic.useCase.GetCurrentUserUseCase
 import org.example.logic.useCase.Validation
-import org.example.logic.utils.*
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -17,37 +19,34 @@ class CreateTaskUseCase(
     private val createAuditLogUseCase: CreateAuditLogUseCase,
     private val projectStateRepository: ProjectStateRepository,
     private val validation: Validation,
-    ) {
-    suspend operator fun invoke(
+) {
+    operator fun invoke(
         name: String,
         projectId: Uuid,
         stateId: Uuid,
     ): Task {
         validation.validateInputNotBlankOrThrow(name)
         val state = getState(stateId)
-        val currentUser = getCurrentUserUseCase()
-        return taskRepository.createTask(
+        val currentUser = getCurrentUserUseCase().blockingGet()
+        val task =
             Task(
                 name = name,
                 stateId = stateId,
                 stateName = state.title,
                 projectId = projectId,
                 addedById = currentUser.id,
-                addedByName = currentUser.username
+                addedByName = currentUser.username,
             )
-        ).also { task ->
-            createAuditLogUseCase.logCreation(
-                entityId = task.id,
-                entityName = task.name,
-                entityType = AuditLog.EntityType.TASK,
-            )
+
+        return taskRepository.createTask(task).doOnSuccess {
+            createAuditLogUseCase
+                .logCreation(
+                    entityId = task.id,
+                    entityName = task.name,
+                    entityType = AuditLog.EntityType.TASK,
+                ).blockingGet()
         }
     }
 
-    private suspend fun getState(
-        stateId: Uuid,
-    ): ProjectState = projectStateRepository.getProjectStateById(stateId).takeIf {
-            it != null
-        } ?: throw TaskStateNotFoundException()
-
+    private fun getState(stateId: Uuid): ProjectState = projectStateRepository.getProjectStateById(stateId).blockingGet()
 }
