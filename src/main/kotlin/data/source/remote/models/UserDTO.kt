ae27d017fbd3ba8efc2c76d19fd93339141ca011
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-18 16:24:00 +0300

    feat: use SerialName for _id field

--- src/main/kotlin/data/source/remote/models/UserDTO.kt
+++ src/main/kotlin/data/source/remote/models/UserDTO.kt
@@ -6,20 +6,21 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
-import org.bson.codecs.pojo.annotations.BsonId
 
 @Serializable
 data class UserDTO(
-    @BsonId val id: String,
+    @SerialName("_id") val id: String,
     val username: String,
     val role: String,
-    val authMethod: AuthenticationMethodDto
+    val authMethod: AuthenticationMethodDto,
 ) {
     @Serializable
     @JsonClassDiscriminator("type")
-    sealed class AuthenticationMethodDto{
+    sealed class AuthenticationMethodDto {
         @Serializable
         @SerialName("password")
-        data class Password(val password: String): AuthenticationMethodDto()
+        data class Password(
+            val password: String,
+        ) : AuthenticationMethodDto()
     }
 }
